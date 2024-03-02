59aaf8e19f04711507d9c37d75b2052f2caa6f02
Alibek Suleimenov <alibek443@gmail.com>
2024-03-02 18:51:09 +0500

    added stub components, updated router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppBody from "./../components/AppBody.vue"
 import AppLogin from "./../components/AppLogin.vue"
+import AppBooks from "./../components/AppBooks.vue";
+import AppBook from "./../components/AppBook.vue";
+import BooksAdmin from "./../components/BooksAdmin.vue";
+import BookEdit from "./../components/BookEdit.vue";
+import AppUsers from "./../components/AppUsers.vue";
+import UserEdit from "./../components/UserEdit.vue";
 
 const routes = [
     {
@@ -12,9 +18,39 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: AppLogin,
-    }
+    },
+    {
+        path: '/books',
+        name: 'Books',
+        component: AppBooks,
+    },
+    {
+        path: '/books/:bookName',
+        name: 'Book',
+        component: AppBook,
+    },
+    {
+        path: '/admin/books',
+        name: 'BooksAdmin',
+        component: BooksAdmin,
+    },
+    {
+        path: '/admin/books/:bookId',
+        name: 'BookEdit',
+        component: BookEdit,
+    },
+    {
+        path: '/admin/users',
+        name: 'AppUsers',
+        component: AppUsers,
+    },
+    {
+        path: '/admin/users/:userId',
+        name: 'UserEdit',
+        component: UserEdit,
+    },
 ];
 
-const router = createRouter({history: createWebHistory(), routes});
+const router = createRouter({ history: createWebHistory(), routes });
 
 export default router;
